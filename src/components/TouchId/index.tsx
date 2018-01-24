8f6aa29823a0d3f49299749052692385aa58e888
2vm <vladimir.matsola@litslink.com>
2018-01-24 11:09:22 +0200

    face ID alert message wont confusing users anymore

--- src/components/TouchId/index.tsx
+++ src/components/TouchId/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Icon } from 'react-native-elements';
-import { Platform } from 'react-native';
+import { Alert } from 'react-native';
 
 import TouchID from 'react-native-touch-id';
 
@@ -14,27 +14,27 @@ interface ITouchIdProps {
 class TouchId extends React.Component<ITouchIdProps> {
 
   render() {
-    return null;
-    // return (
-    //   <Icon
-    //     type="material-icons"
-    //     name="fingerprint"
-    //     size={66}
-    //     {...this.props.iconStyle}
-    //     onPress={this.handlePrint}
-    //   />
-    // );
+    return (
+      <Icon
+        type="material-icons"
+        name="fingerprint"
+        size={66}
+        {...this.props.iconStyle}
+        onPress={this.handlePrint}
+      />
+    );
   }
 
   private handlePrint = (): void => {
     console.log('there will be saga... probably');
-    if (Platform.OS === 'android' && Platform.Version < 23) {
-      TouchID.authenticate().then((r: Object) => {
-        console.log(r, 'success');
-      }).catch((err: Object) => {
-        console.log(err, 'error')
-      })
-    }
+    TouchID.authenticate().then((r: Object) => {
+      // console.log(r, 'success');
+      Alert.alert(
+        `Button added just for make testing of Face / Touch ID available. Will be removed later.`
+      );
+    }).catch((err: Object) => {
+      console.log('error');
+    })
   }
 }
 
