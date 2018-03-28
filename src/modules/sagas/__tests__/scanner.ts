6ea406225b710408c6a6088acb003ba66e67b0c0
2vm <vladimir.matsola@litslink.com>
2018-03-28 11:44:09 +0300

    scanner saga tests

---
+++ src/modules/sagas/__tests__/scanner.ts
@@ -0,0 +1,25 @@
+import { qrCodeWorker } from '../scanner';
+import { put } from 'redux-saga/effects';
+import { navigate } from 'modules/navigator/actions';
+import { navTypes } from '../../../constants';
+
+describe('qrCodeWorker', () => {
+  it('qrCodeWorker => key.length === 64', () => {
+    const action = { type: 'type', payload: 'decryptedValuedecryptedValuedecryptedValuedecryptedValdecrydecry;1;null' };
+    const iterator = qrCodeWorker(action);
+    expect(iterator.next().value).toEqual(put(
+      navigate(navTypes.SIGN_IN, {
+        privateKey: 'decryptedValuedecryptedValuedecryptedValuedecryptedValdecrydecry',
+        ecosystemId: '1'
+      })
+    ));
+    expect(iterator.next().value).toEqual(undefined);
+  });
+
+  it('qrCodeWorker => key.length !== 64', () => {
+    const action = { type: 'type', payload: 'not64LengthKey;1;null' };
+    const iterator = qrCodeWorker(action);
+    iterator.next();
+    expect(iterator.next().value).toEqual(undefined);
+  });
+});
