604e53a1441686bbe05789244524bc0044e73850
2vm <vladimir.matsola@litslink.com>
2018-04-25 16:13:30 +0300

    removed a few useless selectors

--- src/modules/auth/selectors.js
+++ src/modules/auth/selectors.js
@@ -11,11 +11,7 @@ export const hasValidToken = state =>
 
 export const getTokenExpiry = state => state.auth.tokenExpiry;
 
-export const getCurrentAccountAddress = state => state.auth.currentAccountAddress;
-
 export const getCurrentEcosystemId = state => state.auth.currentEcosystemId;
-
-export const getLastLoggedAccount = state => state.auth.lastLoggedAccount || null;
 
 export const getAuthStatus = state => state.auth.isAuthenticated;
 
