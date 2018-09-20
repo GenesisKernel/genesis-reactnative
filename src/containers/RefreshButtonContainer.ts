86860ebb8a0e11b22e1089eb5ed0edb98760aa73
2vm <vladimir.matsola@litslink.com>
2018-09-20 16:51:25 +0300

    Add state.props to container

--- src/containers/RefreshButtonContainer.ts
+++ src/containers/RefreshButtonContainer.ts
@@ -2,7 +2,11 @@ import { connect } from 'react-redux';
 import { actions } from 'modules/page';
 import RefreshButton from 'components/RefreshButton'
 
-const mapStateToProps = (state: any, ownProps: any) => ({ state, ...ownProps });
+const mapStateToProps = (state: any, ownProps: any) => ({
+  historyItems: state.application.history,
+  currentItem: state.application.currentPageId,
+  ...ownProps,
+});
 
 const mapDispatchToProps = (dispatch: any) => ({
   requestPageStarted: (pagePayload: actions.IPagePayload) => {
